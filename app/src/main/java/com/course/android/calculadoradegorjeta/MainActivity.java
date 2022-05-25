edc3d71835cce8a76cdbb4bd5d94805c901b2a0e
faening <cmorais58@gmail.com>
2022-05-24 21:26:41 -0300

    Corrigido um erro na conversão de inputs do projeto.

--- app/src/main/java/com/course/android/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/course/android/calculadoradegorjeta/MainActivity.java
@@ -1,53 +1,57 @@
 package com.course.android.calculadoradegorjeta;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+// @SuppressWarnings("FieldCanBeLocal");
 public class MainActivity extends AppCompatActivity {
-    private EditText rec_valorPedido;
-    private TextView rec_valorTotalPedido, rec_valorTotalGorjeta;
-    private SeekBar rec_percentualGorjeta;
+    private TextView ref_valorPedido, ref_valorTotalpedido, ref_valorTotalGorjeta, ref_valorTotalGeral;
+    private SeekBar ref_percentualGorjeta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rec_valorPedido = findViewById(R.id.InputValorPedido);
-        rec_percentualGorjeta = findViewById(R.id.SeekbarPercentualGorjeta);
-        rec_valorTotalPedido = findViewById(R.id.TxtResultadoTotalPedido);
-        rec_valorTotalGorjeta = findViewById(R.id.TxtResultadoTotalGorjeta);
+        ref_valorPedido = findViewById(R.id.InputValorPedido);
+        ref_valorTotalpedido = findViewById(R.id.TxtResultadoTotalPedido);
+        ref_valorTotalGorjeta = findViewById(R.id.TxtResultadoTotalGorjeta);
+        ref_percentualGorjeta = findViewById(R.id.SeekbarPercentualGorjeta);
+        ref_valorTotalGeral = findViewById(R.id.TxtResultadoTotalGeral);
 
-        // Total -> Pedido
-        Double valorPedido = Double.parseDouble(rec_valorPedido.getText().toString());
+        ref_percentualGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            Double valorPedido = ref_valorPedido.getText().toString().isEmpty() ? 0.0 : Double.parseDouble(ref_valorPedido.getText().toString());
 
-        if (valorPedido > 0.0) {
-            rec_valorTotalPedido.setText(rec_valorPedido.getText().toString());
-        }
-
-        // Total -> Gorjeta
-        rec_percentualGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 if (valorPedido <= 0.0) {
-                    rec_valorTotalGorjeta.setText("0.00");
-                } else {
-                    rec_valorTotalGorjeta.setText(String.valueOf(i));
+                    Toast.makeText(getApplicationContext(),
+                            "Oporra, informa o valor do pedido ai caralho...",
+                            Toast.LENGTH_LONG
+                    ).show();
                 }
+
+                // Calcular valor da gorjeta
+                Double gorjeta = Double.parseDouble(ref_valorPedido.getText().toString()) * ref_percentualGorjeta.getProgress() / 100.0;
+                ref_valorTotalGorjeta.setText(gorjeta.toString());
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Calcular o valor total do pedido
+                Double totalGeral = Double.parseDouble( ref_valorTotalGeral.getText().toString() + ref_valorTotalGorjeta);
+                ref_valorTotalGeral.setText(totalGeral.toString());
+            }
         });
-
-        // Total -> Total geral
 
     }
 
